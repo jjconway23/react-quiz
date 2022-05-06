ff645c89b96add9e2f9c50eed3f821c8bfaa8a1a
Jacob <jacob.peat01@gmail.com>
2022-05-06 12:02:01 +0100

    Add some styling to the body

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default function Homepage(){
-   
-    
+   return (
+   <div className="home-page-container">
+       <h1>Quizzical</h1>
+       <button>Start Game</button>
+   </div>
+   )
 }
