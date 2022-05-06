912874b75bbf0241851073608799874638c576c3
Jacob <jacob.peat01@gmail.com>
2022-05-06 17:00:01 +0100

    fetch data from api and refactor map variable

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from 'react';
 import Homepage from "./components/Homepage"
 import QuestionsContainer from "./components/QuestionsContainer"
 import './App.css';
-import data from "./dummyData"
+// import data from "./dummyData"
 
 function App() {
   const [startGame, setStartGame] = React.useState(false) 
+  const [questionsData,setQuestionsData] = React.useState([])
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&type=multiple")
+        .then(res => res.json())
+        .then(data => setQuestionsData(data.results))
+}, [])
   function startGameBtn(){
     setStartGame( prevState => !prevState)
   }
-  console.log(data.results)
-  const questionDiv = data.results.map( container => {
+  const questionDiv = questionsData.map( container => {
     return <QuestionsContainer question={container.question}/>
-  } )
-
+  } ) 
+console.log(questionsData)
  
   return (
     <div className="App">
