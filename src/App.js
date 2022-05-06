1c21a54d3a3e0474b3d5d8b38c38347b4f33aa1d
Jacob <jacob.peat01@gmail.com>
2022-05-06 13:00:48 +0100

    add questions container and render to the page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Homepage from "./components/Homepage"
+import Questions from "./components/QuestionsContainer"
 import './App.css';
+import QuestionsContainer from './components/QuestionsContainer';
 
 
 function App() {
@@ -10,11 +12,12 @@ function App() {
     setStartGame( prevState => !prevState)
   }
 
+
   return (
     <div className="App">
-      {!startGame && <Homepage
-      startGameBtn={startGameBtn}
-      />}
+      {!startGame && <Homepage startGameBtn={startGameBtn}/>}
+      <QuestionsContainer />
+
     </div>
   );
 }
---
+++ src/components/QuestionsContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function QuestionsContainer(){
+    return(<div className="questions-holder">
+        <h3>Question will go here</h3>
+        <div className="answers-container">
+            <button>Answer one</button>
+            <button>Answer Two</button>
+            <button>Answer Three</button>
+            <button>Answer Four</button>
+        </div>
+
+    </div>
+    )
+}
